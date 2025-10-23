ed25e77a411bbc410bce47aa2834126f5d36a571
Naveen <naveen@Naveens-MacBook-Air.local>
2025-10-24 00:37:58 +1100

    code

--- pipeline/jenkins/build/build.sh
+++ pipeline/jenkins/build/build.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-# Copy the new jar to the build location
-#cp -f /target/devops-integrfation.jar pipeline/jenkins/build/
-
-echo "****************************"
-echo "** Building Docker Image ***"
-echo "****************************"
-
-cd pipeline/jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+FROM eclipse-temurin:17-jdk-focal
+RUN mkdir /app
+COPY *.jar /app/app.jar
+CMD java -jar /app/app.jar
